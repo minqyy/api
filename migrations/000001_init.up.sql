fb4a4b4aab7b0d497f982e0f4476a4fd42ecd257
jus1d <artfa63@gmail.com>
2024-04-08 18:24:45 +0400

    fix: UNIQUE constraint on `password_hash`

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -4,6 +4,6 @@ CREATE TABLE users
 (
     id uuid DEFAULT uuid_generate_v4() NOT NULL UNIQUE,
     email varchar(255) NOT NULL UNIQUE,
-    password_hash varchar(64) NOT NULL UNIQUE,
+    password_hash varchar(64) NOT NULL,
     created_at timestamp DEFAULT now() NOT NULL
 );

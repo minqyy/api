0cfeba0a441ed858eef43e31283e2e8e7874890a
jus1d <artfa63@gmail.com>
2024-04-11 18:30:53 +0400

    chore(#8): Add request duration and body size to request complited log

--- internal/app/middleware/middleware.go
+++ internal/app/middleware/middleware.go
@@ -1,11 +1,13 @@
 package middleware
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/minqyy/api/internal/config"
 	"github.com/minqyy/api/pkg/requestid"
 	"log/slog"
 	"strings"
+	"time"
 )
 
 type Middleware struct {
@@ -25,10 +27,17 @@ func (m *Middleware) RequestLog(ctx *gin.Context) {
 		return
 	}
 
-	m.log.Info("request handled",
+	start := time.Now()
+
+	ctx.Next()
+
+	m.log.Info("request completed",
 		slog.String("request_id", requestid.Get(ctx)),
 		slog.String("method", ctx.Request.Method),
 		slog.String("path", ctx.Request.URL.Path),
 		slog.String("client_ip", ctx.ClientIP()),
+		slog.String("duration", fmt.Sprintf("%v", time.Now().Sub(start))),
+		slog.Int("body_size", ctx.Writer.Size()),
+		slog.Int("status", ctx.Writer.Status()),
 	)
 }
